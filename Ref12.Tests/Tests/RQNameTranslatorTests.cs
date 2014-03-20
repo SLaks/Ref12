41c9a9ad63c7e3d08222b7a3940e29413c0e161d
Schabse Laks <Dev@SLaks.net>
2014-03-20 18:11:52 -0400

    Tests: Move actual test cases to Tests folder

--- Ref12.Tests/Tests/RQNameTranslatorTests.cs
+++ Ref12.Tests/Tests/RQNameTranslatorTests.cs
@@ -26,6 +26,8 @@ namespace Ref12.Tests {
 			Assert.AreEqual("M:System.Collections.Generic.List`1.ConvertAll``1(System.Converter{`0,``0})", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),NsName(Collections),NsName(Generic),AggName(List,TypeVarCnt(1))),MethName(ConvertAll),TypeVarCnt(1),Params(Param(AggType(Agg(NsName(System),AggName(Converter,TypeVarCnt(2))),TypeParams(TyVar(T),TyVar(TOutput))))))"));
 			Assert.AreEqual("M:S.X`1.Y`1.Z``1(`0,`1,``0,System.Tuple{``0,`1,`0})", RQNameTranslator.ToIndexId("Meth(Agg(AggName(S,TypeVarCnt(0)),AggName(X,TypeVarCnt(1)),AggName(Y,TypeVarCnt(1))),MethName(Z),TypeVarCnt(1),Params(Param(TyVar(T)),Param(TyVar(U)),Param(TyVar(V)),Param(AggType(Agg(NsName(System),AggName(Tuple,TypeVarCnt(3))),TypeParams(TyVar(V),TyVar(U),TyVar(T))))))"));
 
+			// TODO: ref parameters
+
 			// The generated RQName from native language services incorrectly omits the first type argument, making this test unpassable.
 			//Assert.AreEqual("M:S.Complex``1(S.X{System.Int32}.Y{``0},S.X{``0}.Y{System.Int32},System.Char**[][0:,0:,0:])", RQNameTranslator.ToIndexId("Meth(Agg(AggName(S,TypeVarCnt(0))),MethName(Complex),TypeVarCnt(1),Params(Param(AggType(Agg(AggName(S,TypeVarCnt(0)),AggName(X,TypeVarCnt(1)),AggName(Y,TypeVarCnt(1))),TypeParams(TyVar(T)))),Param(AggType(Agg(AggName(S,TypeVarCnt(0)),AggName(X,TypeVarCnt(1)),AggName(Y,TypeVarCnt(1))),TypeParams(AggType(Agg(NsName(System),AggName(Int32,TypeVarCnt(0))),TypeParams())))),Param(Array(3,Array(1,Ptr(Ptr(AggType(Agg(NsName(System),AggName(Char,TypeVarCnt(0))),TypeParams()))))))))"));
 		}
