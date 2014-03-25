023523aa68c5615d745ba9774fc3201324fe7803
Schabse Laks <Dev@SLaks.net>
2014-03-25 08:45:25 -0400

    Tests: Reorganize test methods
    Contains no logic changes

--- Ref12.Tests/Tests/RQNameTranslatorTests.cs
+++ Ref12.Tests/Tests/RQNameTranslatorTests.cs
@@ -5,7 +5,7 @@ namespace Ref12.Tests {
 	[TestClass]
 	public class RQNameTranslatorTests {
 		[TestMethod]
-		public void KnownTestCases() {
+		public void IndexIdTestCases() {
 			Assert.AreEqual("E:System.AppDomain.AssemblyLoad", RQNameTranslator.ToIndexId("Event(Agg(NsName(System),AggName(AppDomain,TypeVarCnt(0))),EventName(AssemblyLoad))"));
 			Assert.AreEqual("F:System.Environment.SpecialFolderOption.Create", RQNameTranslator.ToIndexId("Membvar(Agg(NsName(System),AggName(Environment,TypeVarCnt(0)),AggName(SpecialFolderOption,TypeVarCnt(0))),MembvarName(Create))"));
 			Assert.AreEqual("F:System.Runtime.InteropServices.BIND_OPTS.cbStruct", RQNameTranslator.ToIndexId("Membvar(Agg(NsName(System),NsName(Runtime),NsName(InteropServices),AggName(BIND_OPTS,TypeVarCnt(0))),MembvarName(cbStruct))"));
@@ -18,7 +18,9 @@ namespace Ref12.Tests {
 			Assert.AreEqual("T:System.Environment.SpecialFolderOption", RQNameTranslator.ToIndexId("Agg(NsName(System),AggName(Environment,TypeVarCnt(0)),AggName(SpecialFolderOption,TypeVarCnt(0)))"));
 			Assert.AreEqual("T:System.Int32", RQNameTranslator.ToIndexId("Agg(NsName(System),AggName(Int32,TypeVarCnt(0)))"));
 			Assert.AreEqual("T:System.ValueType", RQNameTranslator.ToIndexId("Agg(NsName(System),AggName(ValueType,TypeVarCnt(0)))"));
-
+		}
+		[TestMethod]
+		public void IndexIdMethodParameterTestCases() {
 			Assert.AreEqual("M:System.Environment.SetEnvironmentVariable(System.String,System.String,System.EnvironmentVariableTarget)", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),AggName(Environment,TypeVarCnt(0))),MethName(SetEnvironmentVariable),TypeVarCnt(0),Params(Param(AggType(Agg(NsName(System),AggName(String,TypeVarCnt(0))),TypeParams())),Param(AggType(Agg(NsName(System),AggName(String,TypeVarCnt(0))),TypeParams())),Param(AggType(Agg(NsName(System),AggName(EnvironmentVariableTarget,TypeVarCnt(0))),TypeParams()))))"));
 			Assert.AreEqual("M:System.Linq.ParallelEnumerable.Any``1(System.Linq.ParallelQuery{``0})", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),NsName(Linq),AggName(ParallelEnumerable,TypeVarCnt(0))),MethName(Any),TypeVarCnt(1),Params(Param(AggType(Agg(NsName(System),NsName(Linq),AggName(ParallelQuery,TypeVarCnt(1))),TypeParams(TyVar(TSource))))))"));
 			Assert.AreEqual("M:System.Linq.Queryable.Aggregate``1(System.Linq.IQueryable{``0},System.Linq.Expressions.Expression{System.Func{``0,``0,``0}})", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),NsName(Linq),AggName(Queryable,TypeVarCnt(0))),MethName(Aggregate),TypeVarCnt(1),Params(Param(AggType(Agg(NsName(System),NsName(Linq),AggName(IQueryable,TypeVarCnt(1))),TypeParams(TyVar(TSource)))),Param(AggType(Agg(NsName(System),NsName(Linq),NsName(Expressions),AggName(Expression,TypeVarCnt(1))),TypeParams(AggType(Agg(NsName(System),AggName(Func,TypeVarCnt(3))),TypeParams(TyVar(TSource),TyVar(TSource),TyVar(TSource))))))))"));
--- Ref12.Tests/Tests/VBTests.cs
+++ Ref12.Tests/Tests/VBTests.cs
@@ -21,21 +21,22 @@ namespace Ref12.Tests {
 
 
 		private static IComponentModel componentModel;
+		private static ITextView textView;
+		private static string fileName;
 
 		[ClassInitialize]
 		public static void PrepareSolution(TestContext context) {
 			DTE.Solution.Open(Path.Combine(SolutionDir, "TestBed.sln"));
 
 			componentModel = (IComponentModel)VsIdeTestHostContext.ServiceProvider.GetService(typeof(SComponentModel));
+			fileName = Path.GetFullPath(Path.Combine(SolutionDir, "Basic", "File.vb"));
+			DTE.ItemOperations.OpenFile(fileName).Activate();
+			textView = GetCurentTextView();
 		}
 
 		[TestMethod]
 		[HostType("VS IDE")]
-		public async Task VBResolverTest() {
-			var fileName = Path.GetFullPath(Path.Combine(SolutionDir, "Basic", "File.vb"));
-			DTE.ItemOperations.OpenFile(fileName).Activate();
-			var textView = GetCurentTextView();
-
+		public async Task VBResolverTypeTests() {
 			// Hop on to the UI thread so the language service APIs work
 			await Application.Current.Dispatcher.NextFrame();
 
@@ -43,7 +44,20 @@ namespace Ref12.Tests {
 			Assert.AreEqual("mscorlib", symbol.AssemblyName);
 			Assert.AreEqual("T:System.Lazy`1", symbol.IndexId);
 
-			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("Environment.SetEnvironmentVariable(\"").End - 4);
+			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("AddHandler Microsoft.Win32").End);
+			Assert.IsNull(symbol, "Namespaces should not be resolved");
+
+			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("New List(Of Func").End);
+			Assert.AreEqual("T:System.Func`2", symbol.IndexId);
+		}
+
+		[TestMethod]
+		[HostType("VS IDE")]
+		public async Task VBResolverMemberTests() {
+			// Hop on to the UI thread so the language service APIs work
+			await Application.Current.Dispatcher.NextFrame();
+
+			var symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("Environment.SetEnvironmentVariable(\"").End - 4);
 			Assert.IsFalse(symbol.HasLocalSource);
 			Assert.AreEqual("mscorlib", symbol.AssemblyName);
 			Assert.AreEqual("M:System.Environment.SetEnvironmentVariable(System.String,System.String,System.EnvironmentVariableTarget)", symbol.IndexId);
@@ -57,10 +71,24 @@ namespace Ref12.Tests {
 			Assert.AreEqual("System", symbol.AssemblyName);
 			Assert.AreEqual("E:Microsoft.Win32.SystemEvents.PowerModeChanged", symbol.IndexId);
 
-			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("AddHandler Microsoft.Win32").End);
-			Assert.IsNull(symbol, "Namespaces should not be resolved");
+			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("e = New Exception").End);
+			Assert.AreEqual("M:System.Exception.ctor", symbol.IndexId);
 
-			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("str.Aggregate").End);
+			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("0.ToString").End);
+			Assert.AreEqual("M:System.Int32.ToString", symbol.IndexId);
+
+			// TODO: Wait for the reference source to support operators.
+			//symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("ns +").End);
+			//Assert.AreEqual("M:System.Xml.Linq.XNamespace.op_Addition(System.Xml.Linq.XNamespace,System.String)", symbol.IndexId);
+		}
+
+		[TestMethod]
+		[HostType("VS IDE")]
+		public async Task VBResolverGenericsTests() {
+			// Hop on to the UI thread so the language service APIs work
+			await Application.Current.Dispatcher.NextFrame();
+
+			var symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("str.Aggregate").End);
 			Assert.IsFalse(symbol.HasLocalSource);
 			Assert.AreEqual("System.Core", symbol.AssemblyName);
 			Assert.AreEqual("M:System.Linq.Enumerable.Aggregate``2(System.Collections.Generic.IEnumerable{``0},``1,System.Func{``1,``0,``1})", symbol.IndexId);
@@ -72,12 +100,6 @@ namespace Ref12.Tests {
 			Assert.AreEqual("M:System.Collections.Generic.List`1.ctor(System.Collections.Generic.IEnumerable{`0})", symbol.IndexId);
 			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("New System.Collections.Generic.List").End);
 			Assert.AreEqual("M:System.Collections.Generic.List`1.ctor(System.Collections.Generic.IEnumerable{`0})", symbol.IndexId);
-
-			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("e = New Exception").End);
-			Assert.AreEqual("M:System.Exception.ctor", symbol.IndexId);
-
-			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("New List(Of Func").End);
-			Assert.AreEqual("T:System.Func`2", symbol.IndexId);
 
 			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("o.ConvertAll").End);
 			Assert.AreEqual("M:System.Collections.Generic.List`1.ConvertAll``1(System.Converter{`0,``0})", symbol.IndexId);
@@ -94,19 +116,12 @@ namespace Ref12.Tests {
 			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("iStr.Item").End);
 			Assert.AreEqual("P:System.Collections.ObjectModel.KeyedCollection`2.Item(`0)", symbol.IndexId);
 
-			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("0.ToString").End);
-			Assert.AreEqual("M:System.Int32.ToString", symbol.IndexId);
-
 			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("iInt.Item").End);
 			Assert.AreEqual("P:System.Collections.ObjectModel.Collection`1.Item(System.Int32)", symbol.IndexId);
 
 			symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("Call M(").End - 1);
 			Assert.IsTrue(symbol.HasLocalSource);
 			Assert.AreEqual("M:Basic.File.A`2.B`2.M``1(`2,`0,``0)", symbol.IndexId);
-
-			// TODO: Wait for the reference source to support operators.
-			//symbol = new VBResolver().GetSymbolAt(fileName, textView.FindSpan("ns +").End);
-			//Assert.AreEqual("M:System.Xml.Linq.XNamespace.op_Addition(System.Xml.Linq.XNamespace,System.String)", symbol.IndexId);
 		}
 
 		///<summary>Gets the TextView for the active document.</summary>
