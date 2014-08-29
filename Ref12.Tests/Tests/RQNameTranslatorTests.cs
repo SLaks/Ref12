8baef86fb781ef6c628b7981782c3f21ebde9ee5
SLaks <Dev@SLaks.net>
2014-08-29 18:05:57 -0400

    Fix tests in Dev14
    Always load the TestHostFramework assembly
    that matches the version under test.  Also
    skip RQName tests when unavailable.

--- Ref12.Tests/Tests/RQNameTranslatorTests.cs
+++ Ref12.Tests/Tests/RQNameTranslatorTests.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VSSDK.Tools.VsIdeTesting;
 using SLaks.Ref12.Services;
 
 namespace Ref12.Tests {
 	[TestClass]
 	public class RQNameTranslatorTests {
+		Version dteVersion = new Version(VsIdeTestHostContext.Dte.Version);
+
 		[TestMethod]
 		public void IndexIdTestCases() {
+			if (dteVersion >= new Version(14, 0))
+				Assert.Inconclusive("Dev14 doesn't have RQNameParser");
+
 			Assert.AreEqual("E:System.AppDomain.AssemblyLoad", RQNameTranslator.ToIndexId("Event(Agg(NsName(System),AggName(AppDomain,TypeVarCnt(0))),EventName(AssemblyLoad))"));
 			Assert.AreEqual("F:System.Environment.SpecialFolderOption.Create", RQNameTranslator.ToIndexId("Membvar(Agg(NsName(System),AggName(Environment,TypeVarCnt(0)),AggName(SpecialFolderOption,TypeVarCnt(0))),MembvarName(Create))"));
 			Assert.AreEqual("F:System.Runtime.InteropServices.BIND_OPTS.cbStruct", RQNameTranslator.ToIndexId("Membvar(Agg(NsName(System),NsName(Runtime),NsName(InteropServices),AggName(BIND_OPTS,TypeVarCnt(0))),MembvarName(cbStruct))"));
@@ -21,6 +28,9 @@ namespace Ref12.Tests {
 		}
 		[TestMethod]
 		public void IndexIdMethodParameterTestCases() {
+			if (dteVersion >= new Version(14, 0))
+				Assert.Inconclusive("Dev14 doesn't have RQNameParser");
+
 			Assert.AreEqual("M:System.Environment.SetEnvironmentVariable(System.String,System.String,System.EnvironmentVariableTarget)", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),AggName(Environment,TypeVarCnt(0))),MethName(SetEnvironmentVariable),TypeVarCnt(0),Params(Param(AggType(Agg(NsName(System),AggName(String,TypeVarCnt(0))),TypeParams())),Param(AggType(Agg(NsName(System),AggName(String,TypeVarCnt(0))),TypeParams())),Param(AggType(Agg(NsName(System),AggName(EnvironmentVariableTarget,TypeVarCnt(0))),TypeParams()))))"));
 			Assert.AreEqual("M:System.Linq.ParallelEnumerable.Any``1(System.Linq.ParallelQuery{``0})", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),NsName(Linq),AggName(ParallelEnumerable,TypeVarCnt(0))),MethName(Any),TypeVarCnt(1),Params(Param(AggType(Agg(NsName(System),NsName(Linq),AggName(ParallelQuery,TypeVarCnt(1))),TypeParams(TyVar(TSource))))))"));
 			Assert.AreEqual("M:System.Linq.Queryable.Aggregate``1(System.Linq.IQueryable{``0},System.Linq.Expressions.Expression{System.Func{``0,``0,``0}})", RQNameTranslator.ToIndexId("Meth(Agg(NsName(System),NsName(Linq),AggName(Queryable,TypeVarCnt(0))),MethName(Aggregate),TypeVarCnt(1),Params(Param(AggType(Agg(NsName(System),NsName(Linq),AggName(IQueryable,TypeVarCnt(1))),TypeParams(TyVar(TSource)))),Param(AggType(Agg(NsName(System),NsName(Linq),NsName(Expressions),AggName(Expression,TypeVarCnt(1))),TypeParams(AggType(Agg(NsName(System),AggName(Func,TypeVarCnt(3))),TypeParams(TyVar(TSource),TyVar(TSource),TyVar(TSource))))))))"));
